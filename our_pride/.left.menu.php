b6f8d2a40bff807fe89283d1f7ac70b603b32b20
AleksandrSuh <udarnik71@list.ru>
2019-01-22 23:59:58 +0500

    our_pride папка

---
+++ our_pride/.left.menu.php
@@ -0,0 +1,26 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$aMenuLinks = Array(
+	Array(
+		"Награды",
+		"awards/",
+		Array(), 
+		Array(), 
+		""
+    ),
+	Array(
+		"Дипломы",
+		"diplomas/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Благодарственные письма",
+		"thanks/",
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
